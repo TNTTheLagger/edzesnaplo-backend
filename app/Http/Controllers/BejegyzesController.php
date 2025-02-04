7d93ec86db9bb795e84f85152622c2664fd34757
Hazel <37238787+TNTTheLagger@users.noreply.github.com>
2025-02-04 13:58:44 +0100

    Backemd hopefully done

---
+++ app/Http/Controllers/BejegyzesController.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Bejegyzes;
+use Illuminate\Http\Request;
+
+class BejegyzesController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Bejegyzes::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'gyakorlat' => 'required|string',
+            'ismetlesszam' => 'required|integer|min:1|max:8'
+        ], [
+            'gyakorlat.required' => 'A gyakorlat megadása kötelező!',
+            'ismetlesszam.required' => 'Az ismétlésszám megadása kötelező!',
+            'ismetlesszam.min' => 'Az ismétlésszám minimum 1 lehet!',
+            'ismetlesszam.max' => 'Az ismétlésszám maximum 8 lehet!',
+        ]);
+
+        Bejegyzes::create($request->all());
+
+        return response()->json(['success' => true, 'message' => 'Gyakorlat rögzítve!']);
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BejegyzesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/edzesnaplo', [BejegyzesController::class, 'index']);
+Route::post('/edzesnaplo', [BejegyzesController::class, 'store']);
